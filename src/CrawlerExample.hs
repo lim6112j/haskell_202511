f8c632700e1f57a653f721cf8669d37a66f42865
Ben Lim <lim6112j@gmail.com>
2025-12-09 20:36:54 +0900

    feat: add CrawlerExample.hs
    Add an example Haskell implementation of a web crawler for demonstration purposes.

---
+++ src/CrawlerExample.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric     #-}
+module CrawlerExample where
+
+import Control.Concurrent (getNumCapabilities, threadDelay)
+import Control.Concurrent.Async (mapConcurrently_, async, wait)
+import Control.Concurrent.STM
+import Control.Monad (forever, forM_, void, when)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS (newTlsManager)
+import Network.URI (URI(..), URIAuth(..), parseURI)
+import Text.HTML.Scalpel.Core
+import Data.Maybe (fromMaybe, catMaybes)
+import qualified Data.Text as T
+import GHC.Generics (Generic)
+
+type UrlQueue = TQueue T.Text
+type Visited = TVar (Set T.Text)
+
+newVisited :: IO Visited
+newVisited = newTVarIO Set.empty
+
+markVisited :: Visited -> T.Text -> STM Bool
+markVisited visitedSet ->
+  if Set.member url visitedSet
+    then return False
+    else do
+      writeTVar visited (Set.insert url visitedSet)
+      return True
+
+someFunc11 :: IO ()
+someFunc11 = do
+  putStrLn "someFunc11"
